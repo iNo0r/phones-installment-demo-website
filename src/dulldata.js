30a87ea84e7c2bf3a8da7867872d092aef57472f
King <pic.ner101@gmail.com>
2022-07-26 18:22:48 +0100

    phones dull data file

---
+++ src/dulldata.js
@@ -0,0 +1,156 @@
+const phonesData = [
+  {
+    make: "apple",
+    models: [
+      {
+        model: "iPhone 12 Pro",
+        colors: [
+          {
+            color: "Gold",
+            url: "https://raylo.imgix.net/iphone-12-pro-max-gold.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Graphite",
+            url: "https://raylo.imgix.net/iphone-12-pro-max-graphite.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Pacific Blue",
+            url: "https://raylo.imgix.net/iphone-12-pro-max-blue.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Silver",
+            url: "https://raylo.imgix.net/iphone-12-pro-max-silver.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+        ],
+        storages: ["128GB", "256GB", "512GB"],
+      },
+      {
+        model: "iPhone 13",
+        colors: [
+          {
+            color: "(PRODUCT)RED",
+            url: "https://raylo.imgix.net/iphone-13-product-red.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Blue",
+            url: "https://raylo.imgix.net/iphone-13-blue.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Midnight",
+            url: "https://raylo.imgix.net/iphone-13-midnight.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Starlight",
+            url: "https://raylo.imgix.net/iphone-13-starlight.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+        ],
+        storages: ["128GB", "256GB", "512GB"],
+      },
+      {
+        model: "iPhone 12 Pro Max",
+        colors: [
+          {
+            color: "Gold",
+            url: "https://raylo.imgix.net/iphone-12-pro-max-gold.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Graphite",
+            url: "https://raylo.imgix.net/iphone-12-pro-max-graphite.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Pacific Blue",
+            url: "https://raylo.imgix.net/iphone-12-pro-max-blue.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Silver",
+            url: "https://raylo.imgix.net/iphone-12-pro-max-silver.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+        ],
+        storages: ["128GB", "256GB", "512GB"],
+      },
+    ],
+  },
+  {
+    make: "Samsung",
+    models: [
+      {
+        model: "Galaxy A53",
+        colors: [
+          {
+            // Awesome Black
+            // Awesome Blue
+            // Awesome Peach
+            // Awesome White
+            color: "Awesome Black",
+            url: "https://raylo.imgix.net/galaxy-a53-awesome-black.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Awesome Blue",
+            url: "https://raylo.imgix.net/galaxy-a53-awesome-blue.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Awesome Peach",
+            url: "https://raylo.imgix.net/galaxy-a53-awesome-peach.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Awesome White",
+            url: "https://raylo.imgix.net/galaxy-a53-awesome-white.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+        ],
+        storages: ["128GB"],
+      },
+      {
+        model: "Galaxy S22 Ultra",
+        colors: [
+          {
+            //             Burgundy
+            // Green
+            // Phantom Black
+            // Phantom White
+            color: "Burgundy",
+            url: "https://raylo.imgix.net/galaxy-s22-ultra-burgundy.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Green",
+            url: "https://raylo.imgix.net/galaxy-s22-ultra-green.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Phantom Black",
+            url: "https://raylo.imgix.net/galaxy-s22-ultra-phantom-black.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Phantom White",
+            url: "https://raylo.imgix.net/galaxy-s22-ultra-phantom-white.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+        ],
+        storages: ["128GB", "256GB", "512GB"],
+      },
+      {
+        model: "Galaxy S22 Plus",
+        colors: [
+          {
+            // Green
+            // Phantom Black
+            // Phantom White
+            // Pink Gold
+            color: "Green",
+            url: "https://raylo.imgix.net/galaxy-s22-plus-green.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Phantom Black",
+            url: "https://raylo.imgix.net/galaxy-s22-plus-phantom-black.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Phantom White",
+            url: "https://raylo.imgix.net/galaxy-s22-plus-phantom-white.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+          {
+            color: "Pink Gold",
+            url: "https://raylo.imgix.net/galaxy-s22-plus-pink-gold.png?bg=404EFF&fm=jpg&fit=max&w=1024&h=1024&dpr=1&trim=auto&trim-md=0#00000000",
+          },
+        ],
+        storages: ["128GB", "256GB"],
+      },
+    ],
+  },
+];
