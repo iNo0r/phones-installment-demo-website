7dc94decffbacb737571ccf70cba05377a42af28
King <pic.ner101@gmail.com>
2022-08-01 23:13:20 +0100

    edit router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import WhyUs from "./components/Views/WhyUs.vue";
+import Phones from "./components/Views/Phones.vue";
+import About from "./components/Views/About.vue";
+import BuyAPhone from "./components/Views/BuyAPhone.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -8,6 +11,18 @@ const router = createRouter({
     {
       path: "/",
       component: WhyUs,
+    },
+    {
+      path: "/phones",
+      component: Phones,
+    },
+    {
+      path: "/about",
+      component: About,
+    },
+    {
+      path: "/buy-a-phone",
+      component: BuyAPhone,
     },
   ],
 });
